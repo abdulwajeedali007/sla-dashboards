d77da7b60f5a4e0e19336dbc1cdf347ce35a56d5
abdulwajeedali007 <abdulwajeedali007@gmail.com>
2026-07-10 18:00:26 +0530

    updates

--- src/components/WorkFlow/Nodes/LegendNode/Index.tsx
+++ src/components/WorkFlow/Nodes/LegendNode/Index.tsx
@@ -4,7 +4,7 @@ function Index() {
       <div className="flex justify-start gap-4 items-center mb-4 px-4  ">
         {/* <div className="md:flex md:gap-4"> */}
         <div
-          className={`group flex items-center w-full gap-3 mb-2  md:mb-0 border  px-3 py-1 rounded-full`}
+          className={`group flex items-center gap-3  border  px-3 py-1 rounded-full`}
         >
           <div className="h-10 w-10 border border-gray-400 rounded-full p-1">
             <div className="h-full w-full rounded-full bg-(--success-color)"></div>
@@ -12,7 +12,7 @@ function Index() {
           <p className="text-xl font-bold">Completed</p>
         </div>
         <div
-          className={`group flex items-center gap-3 w-full border  px-3 py-1.5 rounded-full`}
+          className={`group flex items-center gap-3  border  px-3 py-1.5 rounded-full`}
         >
           <div className="h-10 w-10 border border-gray-400 rounded-full p-1">
             <div className="h-full w-full rounded-full bg-(--warning-color)"></div>
@@ -22,7 +22,7 @@ function Index() {
         {/* </div> */}
         {/* <div className="md:flex md:gap-4"> */}
         <div
-          className={`group flex items-center gap-3 mb-2 w-full   border  px-3 py-1 rounded-full `}
+          className={`group flex items-center gap-3 border  px-3 py-1 rounded-full `}
         >
           <div className="h-10 w-10 border border-gray-400 rounded-full p-1">
             <div className="h-full w-full rounded-full bg-gray-400"></div>
@@ -30,7 +30,7 @@ function Index() {
           <p className="text-xl font-bold">New</p>
         </div>
         <div
-          className={`group flex items-center gap-3 w-full border  px-3 py-1.5 rounded-full`}
+          className={`group flex items-center gap-3  border  px-3 py-1.5 rounded-full`}
         >
           <div className="h-10 w-10 border border-gray-400 rounded-full p-1">
             <div className="h-full w-full rounded-full bg-(--danger-color)"></div>
